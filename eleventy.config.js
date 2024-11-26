5eca0e367bb47f51c09f25c1e2e6df62d22cf7cf
Claudio <claudio@ccmdesign.ca>
2024-11-25 17:55:54 -0800

    multiple updates. I lost track of it

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,16 +1,15 @@
-
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/assets/**/*.*");
   eleventyConfig.addPassthroughCopy("./src/css/*.*");
   eleventyConfig.addPassthroughCopy("./src/js/*.js");
   eleventyConfig.addWatchTarget("./src/**/*.njk");
   eleventyConfig.addWatchTarget("./src/**/js/*.js");
   eleventyConfig.addWatchTarget("./src/css/*.css");
-  
+
   return {
     dir: {
       input: "src",
       output: "dist",
     },
   };
-
 };
